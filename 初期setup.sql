f14492c12560da300a78c018cc6ec078686a80cd
Yuki Sakuragi <yuki.sakuragi@snowflake.com>
2025-10-28 20:57:33 +0900

    Update 初期setup.sql

--- 初期setup.sql
+++ 初期setup.sql
@@ -1,11 +1,70 @@
-
-
-
-
 -- script for call center handson
 USE ROLE ACCOUNTADMIN;
+
+-- cross region call
 ALTER ACCOUNT SET CORTEX_ENABLED_CROSS_REGION = 'AWS_US';
 
+-- Prepare warehouse
+-- for voc
+CREATE WAREHOUSE IF NOT EXISTS COMPUTE_WH
+  WAREHOUSE_SIZE = 'SMALL'
+  WAREHOUSE_TYPE = 'STANDARD'
+  AUTO_SUSPEND = 60
+  AUTO_RESUME = TRUE
+  INITIALLY_SUSPENDED = TRUE
+  COMMENT = 'Warehouse for VOC analyze with Cortex LLM';
+
+
+-- Create voc snowflake object
+CREATE DATABASE IF NOT EXISTS VOC_ANALYZE;
+CREATE SCHEMA IF NOT EXISTS AIRLINE_SURVEY;
+
+use schema VOC_ANALYZE.AIRLINE_SURVEY;
+
+CREATE OR REPLACE STAGE VOC_ANALYZE.AIRLINE_SURVEY.HANDSON_RESOURCES
+    DIRECTORY = (ENABLE = TRUE)
+    ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
+    COMMENT = 'Stage for VOC HandsOn';
+
+
+-- Prepare voc resources
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/snow-jp-handson-org/')
+  ENABLED = TRUE;
+
+CREATE OR REPLACE GIT REPOSITORY GIT_INTEGRATION_FOR_VOC_ANALYZE_HANDSON
+  API_INTEGRATION = git_api_integration
+  ORIGIN = 'https://github.com/snow-jp-handson-org/voc_analyze_for_airline.git';
+
+list @GIT_INTEGRATION_FOR_VOC_ANALYZE_HANDSON/branches/main;
+
+COPY FILES INTO @VOC_ANALYZE.AIRLINE_SURVEY.HANDSON_RESOURCES/Data
+  FROM @GIT_INTEGRATION_FOR_VOC_ANALYZE_HANDSON/branches/main/Data/
+  PATTERN = '.*\\.csv.gz$';
+
+-- Create HandsOn Objects 
+CREATE OR REPLACE TABLE VOC_ANALYZE.AIRLINE_SURVEY.SURVEYS (
+  SURVEY_ID         NUMBER(38,0) IDENTITY(1,1) PRIMARY KEY COMMENT '内部サロゲートキー',
+  FREE_TEXT_COMMENT VARCHAR                         COMMENT '自由記述'
+)
+COMMENT = '搭乗アンケート';
+
+COPY INTO VOC_ANALYZE.AIRLINE_SURVEY.SURVEYS
+FROM '@VOC_ANALYZE.AIRLINE_SURVEY.HANDSON_RESOURCES/Data'
+FILE_FORMAT = (TYPE = CSV COMPRESSION = GZIP)
+PATTERN = '.*[.]csv[.]gz'
+ON_ERROR = 'CONTINUE';
+
+CREATE OR REPLACE NOTEBOOK VOC_ANALYZE_for_Airline
+  FROM @GIT_INTEGRATION_FOR_VOC_ANALYZE_HANDSON/branches/main/Notebook
+  MAIN_FILE = 'AI SQL for Airline.ipynb'
+  QUERY_WAREHOUSE = COMPUTE_WH
+  WAREHOUSE = SYSTEM$STREAMLIT_NOTEBOOK_WH;
+
+
+-- Prepare warehouse
+-- for call center
 CREATE OR REPLACE WAREHOUSE call_center_analytics_wh
     WAREHOUSE_SIZE = 'medium'
     WAREHOUSE_TYPE = 'standard'
@@ -14,10 +73,7 @@ CREATE OR REPLACE WAREHOUSE call_center_analytics_wh
     INITIALLY_SUSPENDED = TRUE
     COMMENT = 'Warehouse for call center analytics with Cortex LLM';
 
-
 -- Prepare stages for data, PDFs, and semantic model artifacts.
-
-
 CREATE DATABASE IF NOT EXISTS call_center_analytics_db;
 CREATE SCHEMA IF NOT EXISTS call_center_analytics_db.analytics;
 use schema call_center_analytics_db.analytics;
